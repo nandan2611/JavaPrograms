f950997063937fc304cfa4839eba39526589118c
Hp <Hp@DESKTOP-H5VC2VM>
2025-02-17 17:36:43 +0530

    Initial Commit.

---
+++ java_programs/src/com/demo/java_programs/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.demo.java_programs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!");
+	}
+
+}
